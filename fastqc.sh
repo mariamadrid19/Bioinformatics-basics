85c21c41dd285cc1708b7deaee8364d8cccace47
Alejandro Castellanos <a.castellanoss@uniandes.edu.co>
2022-06-23 20:03:48 -0500

    Updated FastQC script

--- fastqc.sh
+++ fastqc.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
+# AUTHOR: Alejandro Castellanos Sánchez
 
-module load fastqc/0.11.7
+# FastQC basic usage
 
-fastqc Undetermined_S0_R1_001.fastq -o Undetermined_S0_R1
-fastqc Undetermined_S0_R2_001.fastq -o Undetermined_S0_R2
+# Load FastQC
+module load fastqc
+
+# Running FastQC
+fastqc reads_file.fastq -o fastqc-output-dir
+
+# Script for running FastQC automatically for all the files in a direactory named reads
+# FastQC output directories will be stored in a directory named reads-quality. Output directories will have the same name of the reads file
+
+mkdir -p reads-quality                       # This command will create a direcatory named reads-quality and overwrite it if already exists
+for i in reads/*: do
+	filename=$(basename $i)
+	fname=${filename%.*}
+	fastqc $i -o fastqc-output-dir/$fname
+done
